fe9f53e6577394eece43dd7e9bafe4c9d9f2b9c9
vigno88 <nathan.alix88@gmail.com>
2022-03-09 14:16:45 -0500

    Refactor folder structure

---
+++ wilds/configs/model.py
@@ -0,0 +1,41 @@
+model_defaults = {
+    'densenet121': {
+        'model_kwargs': {
+            'pretrained':True,
+        },
+        'target_resolution': (224, 224),
+    },
+    'wideresnet50': {
+        'model_kwargs': {
+            'pretrained':True,
+        },
+        'target_resolution': (224, 224),
+    },
+    'resnet18': {
+        'model_kwargs':{
+            'pretrained':True,
+        },
+        'target_resolution': (224, 224),
+    },
+    'resnet34': {
+        'model_kwargs':{
+            'pretrained':True,
+        },
+        'target_resolution': (224, 224),
+    },
+    'resnet50': {
+        'model_kwargs': {
+            'pretrained': True,
+        },
+        'target_resolution': (224, 224),
+    },
+    'resnet101': {
+        'model_kwargs': {
+            'pretrained': True,
+        },
+        'target_resolution': (224, 224),
+    },
+    'resnet18_ms': {
+        'target_resolution': (224, 224),
+    },
+}
---
+++ wilds/models/initializer.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import os
+import traceback
+
+from models.layers import Identity
+from utils import load
+
+def initialize_model(config, d_out, is_featurizer=False):
+    """
+    Initializes models according to the config
+        Args:
+            - config (dictionary): config dictionary
+            - d_out (int): the dimensionality of the model output
+            - is_featurizer (bool): whether to return a model or a (featurizer, classifier) pair that constitutes a model.
+        Output:
+            If is_featurizer=True:
+            - featurizer: a model that outputs feature Tensors of shape (batch_size, ..., feature dimensionality)
+            - classifier: a model that takes in feature Tensors and outputs predictions. In most cases, this is a linear layer.
+
+            If is_featurizer=False:
+            - model: a model that is equivalent to nn.Sequential(featurizer, classifier)
+
+        Pretrained weights are loaded according to config.pretrained_model_path using either transformers.from_pretrained (for bert-based models)
+        or our own utils.load function (for torchvision models, resnet18-ms, and gin-virtual).
+        There is currently no support for loading pretrained weights from disk for other models.
+    """
+    # If load_featurizer_only is True,
+    # then split into (featurizer, classifier) for the purposes of loading only the featurizer,
+    # before recombining them at the end
+    featurize = is_featurizer or config.load_featurizer_only
+
+    if config.model in ('resnet18', 'resnet34', 'resnet50', 'resnet101', 'wideresnet50', 'densenet121'):
+        if featurize:
+            featurizer = initialize_torchvision_model(
+                name=config.model,
+                d_out=None,
+                **config.model_kwargs)
+            classifier = nn.Linear(featurizer.d_out, d_out)
+            model = (featurizer, classifier)
+        else:
+            model = initialize_torchvision_model(
+                name=config.model,
+                d_out=d_out,
+                **config.model_kwargs)
+
+    elif config.model == 'resnet18_ms':  # multispectral resnet 18
+        from models.resnet_multispectral import ResNet18
+        if featurize:
+            featurizer = ResNet18(num_classes=None, **config.model_kwargs)
+            classifier = nn.Linear(featurizer.d_out, d_out)
+            model = (featurizer, classifier)
+        else:
+            model = ResNet18(num_classes=d_out, **config.model_kwargs)
+
+    else:
+        raise ValueError(f'Model: {config.model} not recognized.')
+
+    # Recombine model if we originally split it up just for loading
+    if featurize and not is_featurizer:
+        model = nn.Sequential(*model)
+
+    # The `needs_y` attribute specifies whether the model's forward function
+    # needs to take in both (x, y).
+    # If False, Algorithm.process_batch will call model(x).
+    # If True, Algorithm.process_batch() will call model(x, y) during training,
+    # and model(x, None) during eval.
+    if not hasattr(model, 'needs_y'):
+        # Sometimes model is a tuple of (featurizer, classifier)
+        if is_featurizer:
+            for submodel in model:
+                submodel.needs_y = False
+        else:
+            model.needs_y = False
+
+    return model
+
+def initialize_torchvision_model(name, d_out, **kwargs):
+    import torchvision
+
+    # get constructor and last layer names
+    if name == 'wideresnet50':
+        constructor_name = 'wide_resnet50_2'
+        last_layer_name = 'fc'
+    elif name == 'densenet121':
+        constructor_name = name
+        last_layer_name = 'classifier'
+    elif name in ('resnet18', 'resnet34', 'resnet50', 'resnet101'):
+        constructor_name = name
+        last_layer_name = 'fc'
+    else:
+        raise ValueError(f'Torchvision model {name} not recognized')
+    # construct the default model, which has the default last layer
+    constructor = getattr(torchvision.models, constructor_name)
+    model = constructor(**kwargs)
+    # adjust the last layer
+    d_features = getattr(model, last_layer_name).in_features
+    if d_out is None:  # want to initialize a featurizer model
+        last_layer = Identity(d_features)
+        model.d_out = d_features
+    else: # want to initialize a classifier for a particular num_classes
+        last_layer = nn.Linear(d_features, d_out)
+        model.d_out = d_out
+    setattr(model, last_layer_name, last_layer)
+
+    return model
