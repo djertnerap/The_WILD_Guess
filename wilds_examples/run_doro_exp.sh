0d122c239d5ff51edf912ea9d0ce228efc9ce029
William Callaghan <wcallag@gmail.com>
2022-05-01 21:23:47 +0000

    repo cleanup

--- wilds_examples/run_doro_exp.sh
+++ wilds_examples/run_doro_exp.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 BASE_DIR="/home/wcallag_gmail_com"
-FRAC="1"
-BASE_NAME="erm_frac_${FRAC}"
-ALG="_doro_30_epoch_alpha0.3_eps0.005"
+FRAC=1
+EPOCHS=30
+ALPHA=0.25
+EPS=0.005
 PROJECT="doro"
 
-TRAIN_NAME="train_${BASE_NAME}${ALG}"
-EVAL_NAME="eval_${BASE_NAME}${ALG}"
+BASE_NAME="erm_frac_${FRAC}"
+ALG="_${PROJECT}_${EPOCHS}_epoch_alpha${ALPHA}_eps${EPS}"
+SUFFIX=""
+
+TRAIN_NAME="train_${BASE_NAME}${ALG}${SUFFIX}"
+EVAL_NAME="eval_${BASE_NAME}${ALG}${SUFFIX}"
 
 LOG_DIR="${BASE_DIR}/Development/exp_logs"
 DATA_DIR="${BASE_DIR}/Data"
@@ -16,38 +21,25 @@ TRAIN=true
 
 if [ ${TRAIN} == true ];
 then
+    echo "Checking if ${LOG_DIR}/${TRAIN_NAME} exists..."
     if [ -d "${LOG_DIR}/${TRAIN_NAME}" ];
     then
         echo "Running Training..."
-        python run_expt.py \
+        python run_will_expt.py \
         -d fmow \
-        --algorithm "doro" \
+        --algorithm=${PROJECT} \
         --doro_alg "cvar_doro" \
-        --alpha 0.3 \
-        --eps 0.005 \
+        --alpha=${ALPHA}\
+        --eps=${EPS} \
         --root_dir /home/wcallag_gmail_com/Data \
         --loader_kwargs "num_workers=8" \
         --loader_kwargs pin_memory=True \
-        --frac 1 \
+        --frac=${FRAC} \
         --use_wandb=true \
         --log_dir="${LOG_DIR}/${TRAIN_NAME}" \
-        --n_epochs=30 \
+        --n_epochs=${EPOCHS} \
         --wandb_kwargs project=${PROJECT} entity=the-wild-guess
-    fi
-else
-    if [ -d "${LOG_DIR}/${EVAL_NAME}" ];
-        then
-        echo "Running Eval..."
-        python run_expt.py \
-        -d fmow \
-        --algorithm ERM \
-        --root_dir /home/wcallag_gmail_com/Data \
-        --loader_kwargs "num_workers=8" \
-        --loader_kwargs pin_memory=True \
-        --frac 1 \
-        --use_wandb=true \
-        --log_dir="${LOG_DIR}/${EVAL_NAME}" \
-        --wandb_kwargs project=${PROJECT} entity=the-wild-guess \
-        --eval_only
+    else
+        echo "${LOG_DIR}/${TRAIN_NAME} does not exist. Create this directory before running experiment"
     fi
 fi
